292f91cbad889359993c0aa3cbb0935dd17a94df
shubhojit <royshubhojit@gmail.com>
2021-01-18 10:01:34 +1100

    commit -m "Uploading newRecipe via API key but no success message."

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
+export const KEY = 'e9ec8cbb-1e9c-4d38-9b19-03d7aee4208f';
+export const MODAL_CLOSE_SEC = 2.5;
